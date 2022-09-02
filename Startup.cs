07f373d844f35c7c4e7d892e632586f1a89d10c9
Arthur de Villiers <arthurdevilliers@gmail.com>
2022-09-02 17:09:59 +0200

    Updated Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMPG323_Project_2___35359099
 {
