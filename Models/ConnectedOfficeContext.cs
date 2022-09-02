5385690945f5e709208cf036867e23d6dd81b7c1
Arthur de Villiers <arthurdevilliers@gmail.com>
2022-09-02 17:32:56 +0200

    Added OpenAPI & Swagger

--- Models/ConnectedOfficeContext.cs
+++ Models/ConnectedOfficeContext.cs
@@ -25,11 +25,7 @@ namespace CMPG323_Project_2___35359099.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("");
-            }
+          
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
